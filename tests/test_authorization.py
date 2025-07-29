8f32085c1c7e600d6f9dcde52fb8768a6d56db18
Yeldos Tokshalov <eldos_123@mail.ru>
2025-07-29 17:50:48 +0500

    6.7 Параметризация Pytest. Практика работы с pytest параметризацией.

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,7 +4,9 @@ from playwright.sync_api import expect, Page
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+@pytest.mark.parametrize("email", ["user.name@gmail.com", "user.name@gmail.com", "  "])
+@pytest.mark.parametrize("password", ["password", "  ", "password"])
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
