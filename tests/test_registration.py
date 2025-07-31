caa4b7fed797ecc8600ded301cb173169259ec13
Yeldos Tokshalov <eldos_123@mail.ru>
2025-08-01 00:15:33 +0500

    7.1 Знакомство с PageObject. Практика работы с Page Object.

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,31 +1,15 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect, Page
+from playwright.sync_api import sync_playwright
+
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):  # Теперь используем фикстуру
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.visit_registration_page("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form_input(email="user@gmail.com", username="username", password="password")
+    registration_page.click_registration_button()
+    dashboard_page.check_dashboard_title_visibility()
 
-    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
-
-    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
-
-    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
-
-    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
-
-
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
-    page = context.new_page()
-
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
